4417c05b78d93df50eef4bfaba63ca681ae435c5
spiderbeg <3368483402@qq.com>
2019-10-22 10:23:09 +0800

    34，添加暴力求解

--- answer/ex34.py
+++ answer/ex34.py
@@ -1,10 +1,19 @@
 # -*- coding: UTF-8 -*-
- 
-for i in range(1,85):
-    if 168 % i == 0:
-        j = 168 / i
-        if  i > j and (i + j) % 2 == 0 and (i - j) % 2 == 0 :
-            m = (i + j) / 2
-            n = (i - j) / 2
-            x = n * n - 100
-            print(x)
+
+# 方法一
+# for i in range(1,85):
+#     if 168 % i == 0:
+#         j = 168 / i
+#         if  i > j and (i + j) % 2 == 0 and (i - j) % 2 == 0 :
+#             m = (i + j) / 2
+#             n = (i - j) / 2
+#             x = n * n - 100
+#             print(x)
+
+# 方法二
+from math import sqrt
+# sprt() 求平方根，返回浮点数，is_integer() 判断浮点数是否为整数
+for i in range(-100,10000):
+    if sqrt(i+100).is_integer() and sqrt(i+100+168).is_integer(): 
+        print(i)
+    
