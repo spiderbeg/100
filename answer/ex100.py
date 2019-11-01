c991639c2a3aa06d7b8e3937661dd1870b97a6d0
spiderbeg <3368483402@qq.com>
2019-11-01 15:17:03 +0800

    修改第100i题

--- answer/ex100.py
+++ answer/ex100.py
@@ -1,8 +1,10 @@
+#公鸡 5 钱 , 母鸡 3 钱, 小鸡 1/3 钱。要求：100只鸡 100文钱
 #coding:utf-8
 def fun():
-    for i in range(0, 100, 3):
-        for j in range(100):
-            if 100 - i - j >= 0 and 5*(100 - i - j) + i/3 + j == 100 and (i/3 + j)%5 == 0:
-                print ('小鸡%d只，母鸡%d只，公鸡%d只'%(i, j, 100 - i - j))
+    for i in range(0, 100, 3): # i 母鸡总价
+        for j in range(0,100,5): # j 公鸡总价
+            s = 100 - i - j # s 小鸡总价
+            if s >= 0 and s*3 + i//3 + j//5 == 100: # 判断鸡的数量等于100
+                print ('小鸡%d只，母鸡%d只，公鸡%d只'%(s*3, i//3, j//5))
 
 fun()
