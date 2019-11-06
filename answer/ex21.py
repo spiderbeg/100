ea1db9cec0a3076230f8867a5a0ced83548d9ede
spiderbeg <3368483402@qq.com>
2019-11-06 13:20:34 +0800

    answer/ex21

--- answer/ex21.py
+++ answer/ex21.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
 if __name__ == '__main__':
-    a = input('请输入4位以上数字:\n')
+    a = input('请输入7及位以上数字:\n')
     if a.isdigit() and len(a) >= 4:
         print('输入 %s，\t输出 %s。' %(a,a[-7:-3]))
