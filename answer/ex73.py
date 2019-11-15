2822af4f550b5369e059ca9744022c13ae501f82
spiderbeg <3368483402@qq.com>
2019-11-15 14:47:07 +0800

    明确73题题目，修改答案

--- answer/ex73.py
+++ answer/ex73.py
@@ -2,10 +2,10 @@ if __name__ == '__main__':
     sum = 4
     s = 4
     for j in range(2,9):
-        print(sum)
         if j <= 2:
-            s *= 7
+            s *= 6
         else:
-            s *= 8
+            s *= (9-j)
+        print(j,s,sum)
         sum += s
     print('sum = %d' % sum)
